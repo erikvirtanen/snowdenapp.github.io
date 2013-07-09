9d48a336bb207099e0ff92082a12b6a28d22a14b
snowdenapp <ian.purton+4@gmail.com>
2013-07-09 14:30:12 +0200

    Update views.js

--- views.js
+++ views.js
@@ -10,13 +10,13 @@ App.Views.ContactsList = Backbone.View.extend({
   initialize: function() {
     this.collection = new App.Models.ContactList([
       {
-          public_key: '02DA857E3544DEEE37648C6D5534779708'
+          public_key: '02ABBF78EA418B278229ECC4572476DD9E'
       },
       {
-          public_key: '02E71B4966BCC099BCA6AF121FDE17B636'
+          public_key: '02157C6A821C953F47FD1E738AA3C3E3F2'
       },
       {
-          public_key: '03C10168066AA5A3DECFB281BFC9155CDE'
+          public_key: '02F03335D64F39DC1204F933C09780F9D9'
       }
     ]);
     this.collection.on('reset', this.render, this);
@@ -41,10 +41,16 @@ App.Views.ShowConversation = Backbone.View.extend({
     return template(context);
   },
 
-  render: function() {
+  events: {
+    'click #send-message': 'doSendMessage',
+  },
+
+  render: function(id) {
+    App.ApplicationStateSingleton.set("current_public_key", id);
     this.$el.html(this.template(
       {
-          public_key: '02DA857E3544DEEE37648C6D5534779708'
+          public_key: id,
+          private_key: App.PrivateKeySingleton.get("private_key"),
       }
     ));
     this.show();
@@ -52,7 +58,21 @@ App.Views.ShowConversation = Backbone.View.extend({
 
   show: function() {
     $('#content').html(this.el);
-  }
+  },
+  
+  doSendMessage: function(){
+    var encrypted = ECDH.encrypt(App.PrivateKeySingleton.get("private_key"),
+      App.ApplicationStateSingleton.get("current_public_key"), 
+      $('#the-message').val());
+      
+    alert($('#the-message').val() + ' ' + App.PrivateKeySingleton.get("private_key") +
+      ' ' + App.ApplicationStateSingleton.get("current_public_key") +
+      ' ' + encrypted);
+      
+    $('#encrypted-message').text(encrypted);
+      
+    return false;
+  },
 });
 
 App.Views.Login = Backbone.View.extend({
@@ -78,8 +98,11 @@ App.Views.Login = Backbone.View.extend({
     $('#content').html(this.el);
   },
   
-  doLogon: function(){
-    App.Router.navigate('contacts', { trigger: true });
+  doLogon: function(event){
+    event.preventDefault(); 
+    var pk = $('#passphrase_input').val();
+    App.PrivateKeySingleton.setPrivateKey(pk);
+    window.location.assign('#contacts');
   },
   
   doRegenerate: function(){
