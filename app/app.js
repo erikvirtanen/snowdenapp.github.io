f276a4be1f07d16532b189d8c7e644bcca22401f
snowdenapp <ian.purton+4@gmail.com>
2013-07-10 12:13:03 +0200

    Update app.js

--- app/app.js
+++ app/app.js
@@ -5,7 +5,7 @@ window.App = {
   Routers: {},
   
 
-  base_url: 'http://kriptapp.herokuapp.com/',
+  base_url: 'http://178.79.174.142',
 
   auth_token: function() {
     return $.cookie('auth_token');
